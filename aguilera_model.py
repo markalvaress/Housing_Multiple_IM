f572720ff7e63659641fa4162ef7ca20af7fe03f
markalvaress <mark.alvarez2@gmail.com>
2024-11-26 11:47:28 +0000

    simulated ldn with aguilera

--- aguilera_model.py
+++ aguilera_model.py
@@ -1,59 +1,62 @@
 import numpy as np
 import scipy.signal
 
-def calculate_new_house_values_conv(house_grid, affluence_grid, r, lambda_):
-    '''
-    Uses a convolution to calculate new house values.
-    '''
+def calculate_num_neighbours(house_grid, r):
+    """Assumes house_grid has -1s for non-houses. Uses that rather than 0 to allow for houses that actually have 0 value."""
+    # split into 1 for has house there and 0 for doesn't
+    house_grid_binary = np.copy(house_grid)
+    house_grid_binary = np.where(house_grid_binary == -1, 0, 1)
+    kernel = np.ones((2*r + 1, 2*r + 1))
+    num_neighbours = scipy.signal.convolve2d(house_grid_binary, kernel, mode='same', boundary="symm")
+    return num_neighbours
+
+def calculate_new_house_values_conv(house_grid, affluence_grid, r, lambda_, calc_num_neighbours = False):
+    """Calculates new house values using a convolution.
+
+    Args:
+        house_grid (np.ndarray): 
+        affluence_grid (np.ndarray): 
+        r (int): Neighbourhood radius
+        lambda_ (float): Modeling parameter
+        calc_num_neighbours (bool, optional): Whether to calculate number of neighbours or to just use kernel size. When the 
+        grid is full (as in the toy model), we can set this to false as there are always the same number of neighbours. Defaults to False.
+
+    Returns:
+        np.ndarray: The new house values
+    """
     house_grid_zeroed = np.copy(house_grid)
     house_grid_zeroed = np.where(house_grid_zeroed == -1, 0, house_grid_zeroed)
 
     # I think we include the house itself in the nbhd calculation?
     kernel = np.ones((2*r + 1, 2*r + 1))
 
+    if calc_num_neighbours:
+        num_neighbours = calculate_num_neighbours(house_grid, r)
+    else:
+        num_neighbours = kernel.size
     # idk if symmetric bdary conditions is the right thing to do
-    nbhd_average = scipy.signal.convolve2d(house_grid_zeroed, kernel, mode='same', boundary="symm") / kernel.size
+    nbhd_average = scipy.signal.convolve2d(house_grid_zeroed, kernel, mode='same', boundary="symm") / num_neighbours
     V_tplus1 = affluence_grid + lambda_*nbhd_average
+    V_tplus1 = np.where(house_grid == -1, -1, V_tplus1) # make sure to keep gaps from before
 
     return V_tplus1
 
-def calculate_neighborhood_average(house_grid, x, y, r):
+def initialise_affluence_and_house_grids(city_grid, affluence_levels, p):
     """
-    Calculate the average house value in the square neighborhood of radius r around (x, y).
+    Initialises affluence and house grids based on the generated city grid.
+    Houses receive random affluence levels and value levels, while roads remain at -1.
     """
-    n, m = house_grid.shape    
-    neighborhood_sum = 0
-    count = 0
-    
-    x_min, y_min = max(0,x-r), max(0,y-r)
-    x_max, y_max = min(n,x+r), min(m,y+r)
-    
-    # Iterate over the neighborhood within the radius
-    for i in range(x_min, x_max):
-        for j in range(y_min, y_max):
-            # Add the neighbor's value to the sum
-            neighborhood_sum += house_grid[i, j]
-            count += 1
+    m,n = city_grid.shape
+    affluence_grid = -1*np.ones((m,n), dtype=float)
+    house_grid = -1*np.ones((m,n), dtype=float)
 
-    # Calculate the average value of the neighborhood
-    neighborhood_average = neighborhood_sum / count
-    return neighborhood_average
+    for i in range(m):
+        for j in range(n):
+            if city_grid[i, j] == 1:  # 1 is our value for houses
+                affluence_grid[i, j] = np.random.choice(
+                    [affluence_levels["rich"], affluence_levels["middle"], affluence_levels["poor"]],
+                    p=p
+                )
+                house_grid[i, j] = np.random.uniform(0,1)  # Mark as a house
 
-def house_price_update_step(point, house_grid, affluence_grid, lambda_, r):
-    x, y = point
-    neighborhood_avg = calculate_neighborhood_average(house_grid, x, y, r)
-    return affluence_grid[x,y] + lambda_ * neighborhood_avg
-
-# def update_values(affluence_grid, house_grid, lambda_, r):
-#     """
-#     Update house values based on the affluence and neighborhood average.
-#     """
-#     m, n = house_grid.shape
-#     grid_pts = [(x,y) for x in range(m) for y in range(n)]
-#     with Pool(8) as p:
-#         new_house_grid = p.map(lambda pt: house_price_update_step(pt, house_grid, affluence_grid, lambda_, r), grid_pts)
-
-#     new_house_grid = new_house_grid.reshape((m,n))
-
-#     return new_house_grid
-
+    return affluence_grid, house_grid
